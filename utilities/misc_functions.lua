456563f9ab295bf69f8bf4ccb44e0d39ce43496f
jammuu <panglexiao@gmail.com>
2025-09-27 11:43:57 -0700

    WEIRD

--- utilities/misc_functions.lua
+++ utilities/misc_functions.lua
@@ -11,33 +11,37 @@ function G.UIDEF.use_and_sell_buttons(card)
     local switch = nil
     local sell = nil
 
-    if card.area and card.area.config.type == 'joker' then
-        if card.config.center.denouement_can_switch then
-            sell = {n=G.UIT.C, config={align = "cr"}, nodes={
-                {n=G.UIT.C, config={ref_table = card, align = "cr",padding = 0.1, r=0.08, minw = 1.25, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, one_press = true, button = 'sell_card', func = 'can_sell_card'}, nodes={
-                    {n=G.UIT.B, config = {w=0.1,h=0.6}},
-                    {n=G.UIT.C, config={align = "tm"}, nodes={
-                    {n=G.UIT.R, config={align = "cm", maxw = 1.25}, nodes={
+    if card.area and card.area == G.pack_cards then
+        use_and_sell_buttons(card)
+        return
+    end
+
+    if card.config.center.denouement_can_switch then
+        sell = {n=G.UIT.C, config={align = "cr"}, nodes={
+            {n=G.UIT.C, config={ref_table = card, align = "cr",padding = 0.1, r=0.08, minw = 1.25, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, one_press = true, button = 'sell_card', func = 'can_sell_card'}, nodes={
+                {n=G.UIT.B, config = {w=0.1,h=0.6}},
+                {n=G.UIT.C, config={align = "tm"}, nodes={
+                {n=G.UIT.R, config={align = "cm", maxw = 1.25}, nodes={
                         {n=G.UIT.T, config={text = localize('b_sell'),colour = G.C.UI.TEXT_LIGHT, scale = 0.4, shadow = true}}
-                    }},
-                    {n=G.UIT.R, config={align = "cm"}, nodes={
+                }},
+                {n=G.UIT.R, config={align = "cm"}, nodes={
                         {n=G.UIT.T, config={text = localize('$'),colour = G.C.WHITE, scale = 0.4, shadow = true}},
                         {n=G.UIT.T, config={ref_table = card, ref_value = 'sell_cost_label',colour = G.C.WHITE, scale = 0.55, shadow = true}}
-                    }}
-                    }}
-                }},
+                }}
+                }}
+            }},
+        }}
+
+        switch = {n=G.UIT.C, config={align = "cr"}, nodes={
+            {n=G.UIT.C, config={ref_table = card, align = "cr",maxw = 1.25, padding = 0.1, r=0.08, minw = 1.25, minh = (card.area and card.area.config.type == 'joker') and 0 or 1, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, one_press = false, button = 'switch_state', func = 'can_switch_state'}, nodes={
+                {n=G.UIT.B, config = {w=0.1,h=0.6}},
+                {n=G.UIT.T, config={text = "Switch",colour = G.C.UI.TEXT_LIGHT, scale = 0.55, shadow = true}}
+            }}
             }}
 
-            switch = {n=G.UIT.C, config={align = "cr"}, nodes={
-                {n=G.UIT.C, config={ref_table = card, align = "cr",maxw = 1.25, padding = 0.1, r=0.08, minw = 1.25, minh = (card.area and card.area.config.type == 'joker') and 0 or 1, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, one_press = false, button = 'switch_state', func = 'can_switch_state'}, nodes={
-                    {n=G.UIT.B, config = {w=0.1,h=0.6}},
-                    {n=G.UIT.T, config={text = "Switch",colour = G.C.UI.TEXT_LIGHT, scale = 0.55, shadow = true}}
-                }}
-                }}
-
-            local t = {
-                n=G.UIT.ROOT, config = {padding = 0, colour = G.C.CLEAR}, nodes={
-                {n=G.UIT.C, config={padding = 0.15, align = 'cl'}, nodes={
+        local t = {
+            n=G.UIT.ROOT, config = {padding = 0, colour = G.C.CLEAR}, nodes={
+            {n=G.UIT.C, config={padding = 0.15, align = 'cl'}, nodes={
                 {n=G.UIT.R, config={align = 'cl'}, nodes={
                     sell
                 }},
@@ -45,9 +49,9 @@ function G.UIDEF.use_and_sell_buttons(card)
                     switch
                 }},
                 }},
-            }}
-            return t
-        else return use_and_sell_buttons(card) end
+        }}
+        use_and_sell_buttons(card)
+        return t
     end
 
     use_and_sell_buttons(card)
